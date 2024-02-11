3e9c845425045e436b04710958ba4b7d6a8fb1f3
jaenelle isidro <jaenelleisidro@gmail.com>
2024-02-11 14:36:08 +0800

    fix - dettached docker run. permission fix. fixed current directory and shell script name

--- docker_pull_and_run.sh
+++ docker_pull_and_run.sh
@@ -1,5 +1,12 @@
+#this prevent error "permission denied while trying to connect to the Docker daemon"
+sudo chmod 666 /var/run/docker.sock
+
 echo "$DOCKER_PASSWORD" | docker login --username jaenelleisidro --password-stdin
 
 docker pull jaenelleisidro/test_cicd_nodejs:main
-docker run  -p 3000:3000 jaenelleisidro/test_cicd_nodejs:main
-pause
+
+
+
+docker rmi jaenelleisidro/test_cicd_nodejs:main
+
+docker run -p 3000:3000  -d jaenelleisidro/test_cicd_nodejs:main
